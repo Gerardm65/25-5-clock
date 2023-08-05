3ff0fa42bb0ec47c18009401a2ccc7cefe7821bd
Gerardo David Moratinos Meza <gmoratinos8@outlook.es>
2023-08-04 18:33:31 -0700

    Css structure made

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <div className="clock__container"></div>
+        <div className="Main__title">25 + 5 Clock</div>
+        <div className="Break__container">
+          <div className="Break__length">hola </div>
+          <div className="Session__length">mundo</div>
+        </div>
+        <div className="timer__container">
+          <div className="timer">
+            <div className="timer__title"></div>
+            <div className="timer__display"></div>
+          </div>
+        </div>
+        <div className="control__container">
+          <div className="timer__control">aaaaaaaaaaaaaa</div>
+          <div className="timer__author">eeeeeeeeeee</div>
+        </div>
       </header>
     </div>
   );
